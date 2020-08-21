88b73823d05a232b96fe5ee271f1b3a935923c96
ZeeshanShaikh <zeeshanshaikhonline@gmail.com>
2020-08-22 02:38:08 +0530

    update comparator package

--- src/main/java/CoreJava/TestComparable.java
+++ src/main/java/CoreJava/TestComparable.java
@@ -1,5 +1,6 @@
+package CoreJava;
+
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
--- src/main/java/Java_8/UsingComparator.java
+++ src/main/java/Java_8/UsingComparator.java
@@ -15,7 +15,7 @@ public class UsingComparator {
         //str.stream().distinct().sorted(Comparator.naturalOrder()).forEach(System.out::println);
 
         //reverse sort 1
-        //str.stream().distinct().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        str.stream().distinct().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
         //reverse sort 2
         str.stream().distinct().sorted(Comparator.comparing(x -> x.length())).forEach(System.out::println);
