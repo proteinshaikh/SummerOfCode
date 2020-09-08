2381fe6db072e49ee9b150b3592fb9e7a798f3a6
ZeeshanShaikh <zeeshanshaikhonline@gmail.com>
2020-09-09 01:13:34 +0530

    update string equals

--- src/main/java/problems/EqualsAndEquals.java
+++ src/main/java/problems/EqualsAndEquals.java
@@ -7,11 +7,17 @@ public class EqualsAndEquals {
         String s3 = s1;
         String s4 = new String("Sachin");
         String s5 = new String("Sachin");
-        System.out.println("s1==s2 : "+ (s1==s2));
-        System.out.println("s1.equals(s2) : "+s1.equals(s2));
-        System.out.println("s1==s3 : "+ (s1==s3));
-        System.out.println("s4==s5 : "+ (s4==s5));
-        System.out.println("s4.equals(s3) : "+s4.equals(s3));
-        System.out.println("s5.equals(s4) : "+s5.equals(s4));
+        Character c1 = new Character('a');
+        Character c2 = new Character('a');
+        System.out.println("s1==s2 : " + (s1 == s2));
+        System.out.println("s1.equals(s2) : " + s1.equals(s2));
+        System.out.println("s1==s3 : " + (s1 == s3));
+        System.out.println("s1.equals(s3) : " + s1.equals(s3));
+        System.out.println("s4==s3 : " + (s4 == s3));
+        System.out.println("s4.equals(s3) : " + s4.equals(s3));
+        System.out.println("s4==s5 : " + (s4 == s5));
+        System.out.println("s5.equals(s4) : " + s5.equals(s4));
+        System.out.println("c1==c2 : " + (c1 == c2));
+        System.out.println("c1.equals(c2) : " + c1.equals(c2));
     }
 }
