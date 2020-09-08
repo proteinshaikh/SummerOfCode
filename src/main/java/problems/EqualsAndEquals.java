f72598dfed7d6bccbde14d26991121961930c550
ZeeshanShaikh <zeeshanshaikhonline@gmail.com>
2020-09-09 01:07:05 +0530

    update string equals

--- src/main/java/problems/EqualsAndEquals.java
+++ src/main/java/problems/EqualsAndEquals.java
@@ -6,9 +6,12 @@ public class EqualsAndEquals {
         String s2 = "Sachin";
         String s3 = s1;
         String s4 = new String("Sachin");
+        String s5 = new String("Sachin");
         System.out.println("s1==s2 : "+ (s1==s2));
         System.out.println("s1.equals(s2) : "+s1.equals(s2));
         System.out.println("s1==s3 : "+ (s1==s3));
+        System.out.println("s4==s5 : "+ (s4==s5));
         System.out.println("s4.equals(s3) : "+s4.equals(s3));
+        System.out.println("s5.equals(s4) : "+s5.equals(s4));
     }
 }
