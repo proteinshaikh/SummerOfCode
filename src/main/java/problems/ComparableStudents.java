a5c64b279c5734df8d25b09bcfab65ddb5a8401a
Zeeshan <zeeshanshaikhonline@gmail.com>
2020-09-22 03:32:45 +0530

    comparator saga

---
+++ src/main/java/problems/ComparableStudents.java
@@ -0,0 +1,89 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+//sort by grade in asc order and if grade is same sort by name
+public class ComparableStudents {
+
+    public static void main(String[] args) {
+        List<Students> students = new ArrayList<>();
+        students.add(new Students(1, "zeeshan", 27));
+        students.add(new Students(2, "zeeshan", -3));
+        students.add(new Students(3, "zeeshan", 4));
+        students.add(new Students(4, "zeeshan", 6));
+        students.add(new Students(5, "zeeshan", 310));
+        students.add(new Students(6, "zeeshan", 99));
+
+        //using java 7
+        Collections.sort(students);
+        System.out.println(students);
+
+
+    }
+}
+
+class Students implements Comparable<Students> {
+    private int rollno;
+    private String name;
+    private int grade;
+
+    public Students(int rollno, String name, int grade) {
+        this.rollno = rollno;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "rollno=" + rollno +" "+ "name=" + name +" "+ "grade=" + grade + "\n";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rollno, name, grade);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Students students = (Students) o;
+        return rollno == students.rollno && grade == students.grade && Objects.equals(name, students.name);
+    }
+
+    public int compareTo(Students s) {
+        if (this.grade != s.grade) {
+            return this.grade - s.grade;
+        } else {
+            return this.name.compareTo(s.name);
+        }
+
+    }
+}
---
+++ src/main/java/problems/FruitsComparator.java
@@ -0,0 +1,103 @@
+package problems;
+//sort by name and price
+
+import java.util.*;
+
+public class FruitsComparator {
+    public static void main(String[] args) {
+        List<Fruits> fruits=new ArrayList<>();
+        fruits.add(new Fruits(1,"mango", 45.5,"India"));
+        fruits.add(new Fruits(2,"avocado", 100.10,"usa"));
+        fruits.add(new Fruits(3,"pineaple", 13.65163,"canada"));
+        fruits.add(new Fruits(4,"Apple", 44,"India"));
+        fruits.add(new Fruits(5,"Sugarcane", 13.65165,"england"));
+        fruits.add(new Fruits(6,"Papaya", 100.88,"Russia"));
+
+        Collections.sort(fruits, new SortByName());
+        System.out.println(fruits);
+        Collections.sort(fruits, new SortByPrice().reversed());
+        System.out.println(fruits);
+
+    }
+}
+
+class Fruits {
+    private int id;
+    private String name;
+    private double price;
+    private String season;
+
+    public Fruits(int id, String name, double price, String season) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.season = season;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == null) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruits fruits = (Fruits) o;
+        return id == fruits.id && Objects.equals(name, fruits.name) && price == fruits.price && Objects.equals(season, season == fruits.season);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, season);
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + "   name = " + name + "   price = " + price + "   season = " + season + "\n";
+    }
+}
+
+class SortByName implements Comparator<Fruits> {
+    @Override
+    public int compare(Fruits f, Fruits f1) {
+        return f.getName().compareTo(f1.getName());
+
+    }
+}
+
+
+class SortByPrice implements Comparator<Fruits> {
+    @Override
+    public int compare(Fruits f, Fruits f1) {
+        return Double.compare(f.getPrice(), f1.getPrice());
+    }
+}
