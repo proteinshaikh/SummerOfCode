11fd3d27337e6fceb8bbb9a75946dbb71e3624a3
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2020-12-24 04:02:57 +0530

    Adding linear search
    Adding linear search

---
+++ src/main/java/Algorithms/LinearSearch.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		int arr[] = { 5, 7, 2, 4, 9 };
+		boolean flag = false;
+		int findThis = 10;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == findThis) {
+				flag = true;
+			}
+		}
+		System.out.println("Number found --> " + flag);
+	}
+}
