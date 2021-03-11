fc30ae3e739b55264f0d233df440aa84396e30d6
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-03-12 05:17:56 +0530

    format enumaps code

--- src/main/java/CoreJava/EnumMaps.java
+++ src/main/java/CoreJava/EnumMaps.java
@@ -8,15 +8,21 @@ public class EnumMaps {
     }
 
     public static void main(String[] args) {
+
         EnumMap<Color, Integer> color1 = new EnumMap<Color, Integer>(Color.class);
+
         color1.put(Color.RED, 1);
         color1.put(Color.BLUE, 2);
         color1.put(Color.GREEN, 3);
         color1.put(Color.YELLOW, 4);
+
         System.out.println("color 1 are : " + color1);
+
         EnumMap<Color, Integer> color2 = new EnumMap<Color, Integer>(Color.class);
+
         color2.putAll(color1);
         color2.put(Color.ORANGE, 5);
+
         System.out.println("color 2 are : " + color2);
     }
 }
