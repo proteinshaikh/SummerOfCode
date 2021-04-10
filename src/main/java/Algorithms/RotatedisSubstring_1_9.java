d366b0f455f11a812d505f3dacf524569b2ba93d
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-04-10 06:02:05 +0530

    Create RotatedisSubstring_1_9.java

---
+++ src/main/java/Algorithms/RotatedisSubstring_1_9.java
@@ -0,0 +1,35 @@
+package Algorithms;
+
+public class RotatedisSubstring_1_9 {
+    public static void main(String[] args) {
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+
+
+    }
+
+    static int isRotation(String s1, String s2) {
+        int len = s1.length();
+        if (len == s2.length() && len > 0) {
+            String s1s1 = s1 + s1;
+            return isSubstring(s2, s1s1);
+        }
+        return -1;
+    }
+
+    private static int isSubstring(String z1, String z2) {
+        int len1 = z1.length();
+        int len2 = z2.length();
+
+        for (int i = 0; i <= len2 - len1; i++) {
+            int j;
+            for (j = 0; j < len1; j++)
+                if (z2.charAt(i + j) != z1.charAt(j))
+                    break;
+            if (j == len1)
+                return i;
+
+        }
+        return -1;
+
+    }
+}
