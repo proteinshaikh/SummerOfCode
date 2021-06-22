d49f15bd9157357c5ff13d497586670b0cb8ba13
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-06-22 05:58:27 +0530

    Update Sorted_Merge_10_1.java

--- src/main/java/Algorithms/problems/Sorted_Merge_10_1.java
+++ src/main/java/Algorithms/problems/Sorted_Merge_10_1.java
@@ -3,27 +3,26 @@ package Algorithms.problems;
 public class Sorted_Merge_10_1 {
 
     public static void main(String[] args) {
-        Sorted_Merge_10_1 sm=new Sorted_Merge_10_1();
-        System.out.println(sm.merge(new int[]{1}, new int[]{2},0,0 ));
+        Sorted_Merge_10_1 sm = new Sorted_Merge_10_1();
+        System.out.println(sm.merge(new int[]{1}, new int[]{2}, 0, 0));
 
     }
 
-    public boolean merge(int [] a, int [] b, int lastA, int lastB){
-        int indexA=lastA-1;
-        int indexB=lastB-1;
-        int indexMerged=lastB+lastA-1;
+    public boolean merge(int[] a, int[] b, int lastA, int lastB) {
+        int indexA = lastA - 1;
+        int indexB = lastB - 1;
+        int indexMerged = lastB + lastA - 1;
 
-        while(indexB>0){
-            if(indexA>=0 && a[indexA]> b[indexB]){
+        while (indexB > 0) {
+            if (indexA >= 0 && a[indexA] > b[indexB]) {
                 a[indexMerged] = a[indexA];
                 indexA--;
-            }else {
-                a[indexMerged]= b[indexB];
+            } else {
+                a[indexMerged] = b[indexB];
                 indexB--;
             }
             indexMerged--;
         }
-
         return true;
     }
 
