5b64a94e9ee51e6fcf2e7821e8406c101e86d584
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-08-20 04:51:37 +0530

    Update FindDuplicateUsingStreams.java

--- src/main/java/Java8/FindDuplicateUsingStreams.java
+++ src/main/java/Java8/FindDuplicateUsingStreams.java
@@ -7,13 +7,13 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
-* input: 4, 3, 8, 6, 4, 6, 6, 7, 3, 4, 4
-* output: 3 - 2, 4 - 4, 6 - 3, 7 - 1, 8 - 1
-*
-*
-* input: "bob", "bob", "dan", "dan", "rob"
-* output: dan - 2, rob - 1, bob - 2
-* */
+ * input: 4, 3, 8, 6, 4, 6, 6, 7, 3, 4, 4
+ * output: 3 - 2, 4 - 4, 6 - 3, 7 - 1, 8 - 1
+ *
+ *
+ * input: "bob", "bob", "dan", "dan", "rob"
+ * output: dan - 2, rob - 1, bob - 2
+ * */
 public class FindDuplicateUsingStreams {
     public static void main(String[] args) {
         List<Integer> numList = Arrays.asList(4, 3, 8, 6, 4, 6, 6, 7, 3, 4, 4);
@@ -22,11 +22,11 @@ public class FindDuplicateUsingStreams {
         Map<Integer, Long> output = numList.stream().
                 collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         output.forEach((key, value) ->
-                System.out.print(key + " - " + value + ", "));
+                System.out.println(key + " - " + value));
 
         Map<String, Long> output2 = strList.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         output2.forEach((key, value) ->
-                System.out.print(key + " - " + value + ", "));
+                System.out.println(key + " - " + value));
     }
 }
