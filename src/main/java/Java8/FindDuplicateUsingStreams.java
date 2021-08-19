56961c829c5b4a0ce6d148e0390b18befbca9edd
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-08-20 04:39:02 +0530

    Update FindDuplicateUsingStreams.java

--- src/main/java/Java8/FindDuplicateUsingStreams.java
+++ src/main/java/Java8/FindDuplicateUsingStreams.java
@@ -6,6 +6,14 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/*
+* input: 4, 3, 8, 6, 4, 6, 6, 7, 3, 4, 4
+* output: 3 - 2, 4 - 4, 6 - 3, 7 - 1, 8 - 1
+*
+*
+* input: "bob", "bob", "dan", "dan", "rob"
+* output: dan - 2, rob - 1, bob - 2
+* */
 public class FindDuplicateUsingStreams {
     public static void main(String[] args) {
         List<Integer> numList = Arrays.asList(4, 3, 8, 6, 4, 6, 6, 7, 3, 4, 4);
@@ -14,11 +22,11 @@ public class FindDuplicateUsingStreams {
         Map<Integer, Long> output = numList.stream().
                 collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         output.forEach((key, value) ->
-                System.out.println(key + " " + value));
+                System.out.print(key + " - " + value + ", "));
 
         Map<String, Long> output2 = strList.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         output2.forEach((key, value) ->
-                System.out.println(key + " " + value));
+                System.out.print(key + " - " + value + ", "));
     }
 }
