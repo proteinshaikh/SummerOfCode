2f9790fd9d36cd662b10e2b01f79168f7e800fd6
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-11-18 03:18:43 +0530

    Create GCD.java

---
+++ src/main/java/Algorithms/problems/GCD.java
@@ -0,0 +1,35 @@
+package Algorithms.problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(getGcd(54, 36));
+    }
+
+    static List<Integer> getDivisorList(int number) {
+
+        List<Integer> listOfDivisors = new ArrayList<>();
+
+        for (int i = 1; i <= number; i++) {
+            int remainder = number % i;
+
+            if (remainder == 0) {
+                listOfDivisors.add(i);
+            }
+        }
+        return listOfDivisors;
+    }
+
+    static int getGcd(int m, int n) {
+        List<Integer> mList = getDivisorList(m);
+        List<Integer> nList = getDivisorList(n);
+
+        List<Integer> list = mList.stream().filter(nList::contains).collect(Collectors.toList());
+        return Collections.max(list);
+    }
+}
