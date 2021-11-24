1e7805997024535f5d1ab3745baafcfe754f0c36
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-11-25 03:50:42 +0530

    optimise singleton

---
+++ src/main/java/statics/IndiaStatic.java
@@ -0,0 +1,9 @@
+package statics;
+
+public class IndiaStatic {
+
+    static String getMp(String mpName, String Constituency) {
+        return "MP " + mpName + " belongs to " + Constituency + ".";
+
+    }
+}
---
+++ src/main/java/statics/MahaStatic.java
@@ -0,0 +1,7 @@
+package statics;
+
+public class MahaStatic extends IndiaStatic{
+    public static void main(String[] args) {
+        System.out.println(getMp("RG", "Amethi"));
+    }
+}
