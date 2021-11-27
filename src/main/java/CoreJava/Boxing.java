4b8a01984d5f27a6b69d586c34f34937d4678332
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-11-28 02:13:07 +0530

    Create Boxing.java

---
+++ src/main/java/CoreJava/Boxing.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+public class Boxing {
+    public static void main(String[] args) {
+        Integer integer = 4; //boxing
+
+        int i = integer; //unboxing
+        //OR  Autoboxing (done by JVM)
+    }
+}
