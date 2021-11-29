8efc0205aeb491c8992d0f53fe2118347d8333d6
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-11-30 01:37:40 +0530

    Update ArraysSample.java

--- src/main/java/CoreJava/ArraysSample.java
+++ src/main/java/CoreJava/ArraysSample.java
@@ -8,12 +8,13 @@ public class ArraysSample {
         int[] ints = new int[]{1, 2, 3, 4,};
 
         System.out.println("original length of array = " + ints.length);
-        int[] ints2 = Arrays.copyOf(ints, 10);
+        int[] ints2 = Arrays.copyOf(ints, 5);
+        ints2[4] = 7;
 
         System.out.println("New length of array = " + ints2.length);
         int i = 0;
-        while (i < ints.length) {
-            System.out.print(ints[i] + " ");
+        while (i < ints2.length) {
+            System.out.print(ints2[i] + " ");
             i++;
         }
 
