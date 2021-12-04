08a12b1970edf64ef9dace0d6a3be519f7dadf74
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2021-12-05 03:58:20 +0530

    Create ThreadJoins.java

---
+++ src/main/java/CoreJava/ThreadJoins.java
@@ -0,0 +1,36 @@
+package CoreJava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class JoinThreads implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+    }
+}
+
+class JoinThreads2 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Running thread number: " + Thread.currentThread().getId() + " from " + this.getClass().toString().replace("CoreJava.", ""));
+    }
+}
+
+public class ThreadJoins {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(new JoinThreads());
+        thread.start();
+        thread.join();
+        Thread thread2 = new Thread(new JoinThreads2());
+        thread2.start();
+        // thread2.join();
+
+    }
+}
