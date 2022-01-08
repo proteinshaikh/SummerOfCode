b0d8bf04ce5641fe32b10e5d67f1fc05e2d36589
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-01-09 05:22:31 +0530

    Create ImmutableArrayList.java

---
+++ src/main/java/CoreJava/ImmutableArrayList.java
@@ -0,0 +1,30 @@
+package CoreJava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ImmutableArrayList {
+    public static void main(String[] args) {
+        testImmutableArraylist();
+
+    }
+
+    static void testImmutableArraylist() {
+        List<String> list = Arrays.asList("Zeeshan", "Akram", "Shaikh");
+        System.out.println(list.toString());
+
+        System.out.println("Adding elemts in immutable arraylist...");
+
+        List<String> immutableList = Collections.unmodifiableList(list);
+        try {
+            list.add("Test");
+        } catch (UnsupportedOperationException e) {
+            System.out.println("UnsupportedOperationException caught while adding elements in arraylist!");
+        } catch (Exception ex) {
+            System.out.println("Exception Caught : " + ex);
+        }
+
+    }
+}
