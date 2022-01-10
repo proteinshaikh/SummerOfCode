e629c85054d36fc7bcd1cea0763214f03013ef5a
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-01-11 02:24:11 +0530

    Create IterateHashmap.java

---
+++ src/main/java/CoreJava/IterateHashmap.java
@@ -0,0 +1,53 @@
+package CoreJava;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IterateHashmap {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        try {
+            map.put(1, "Zeeshan");
+            map.put(2, "Zeeshan");
+            map.put(3, "Zeeshan");
+            map.put(4, "Zeeshan");
+            map.put(5, "Zeeshan");
+            map.put(null, "Zeeshan");
+            map.put(null, "Zeeshan");
+        } catch (Exception e) {
+            System.out.println("Exception while adding key values in hashmap " + e);
+        }
+
+        //iterate over keys
+        try {
+            for (Integer integer : map.keySet()) {
+                int key = integer;
+                System.out.println(key);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception initerate over keys " + e);
+        }
+
+        //iterate over values
+        try {
+            for (String val : map.values()) {
+                String str = val;
+                System.out.println(val);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception initerate over values " + e);
+        }
+
+        //Iterate over hashmap
+        try {
+            for (Map.Entry<Integer, String> maps : map.entrySet()) {
+                int key = maps.getKey();
+                String value = maps.getValue();
+                System.out.println(key + " : " + value);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception initerate over hashmap " + e);
+        }
+
+    }
+}
