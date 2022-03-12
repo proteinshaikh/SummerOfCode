88a973d9877d032c6426b75fdd37c346915b82f4
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-03-13 03:11:29 +0530

    Update Pyramid.java

--- src/main/java/Algorithms/problems/Pyramid.java
+++ src/main/java/Algorithms/problems/Pyramid.java
@@ -13,6 +13,7 @@ public class Pyramid {
                 System.out.print("# ");
             }
             System.out.println();
+            System.out.println("zeeshan akram shaikh");
         }
     }
 }
