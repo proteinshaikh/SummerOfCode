9add7310e7a87d1e847631c3fbdd3e4c61569209
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-04-25 05:53:13 +0530

    this keyword in java

---
+++ src/main/java/CoreJava/Access.java
@@ -0,0 +1,27 @@
+package CoreJava;
+
+public class Access {
+    private int x = 0;
+    private int y = 0;
+
+    public static void main(String[] args) {
+        Access access = new Access();
+        access.printThis(1, 2);
+        System.out.println();
+        access.printThat(3, 4);
+    }
+
+    private void printThis(int x, int y) {
+        x = x;
+        y = y;
+        System.out.println("printThis method invoked");
+        System.out.println("x: " + this.x + " " + " y: " + this.y);
+    }
+
+    private void printThat(int x, int y) {
+        this.x = x;
+        this.y = y;
+        System.out.println("printThat method invoked");
+        System.out.println("x: " + this.x + " " + " y: " + this.y);
+    }
+}
