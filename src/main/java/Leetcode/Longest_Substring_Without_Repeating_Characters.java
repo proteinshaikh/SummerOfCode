65d03185b0f89c445bfbd431401b130845b46930
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-04-29 05:38:18 +0530

    leetcode

---
+++ src/main/java/Leetcode/Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,37 @@
+package Leetcode;
+
+public class Longest_Substring_Without_Repeating_Characters {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abc"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkRepetition(s, i, j)) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private static boolean checkRepetition(String s, int start, int end) {
+        int[] chars = new int[128];
+
+        for (int i = start; i <= end; i++) {
+            char c = s.charAt(i);
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/Leetcode/Roman_to_Integer.java
@@ -0,0 +1,48 @@
+package Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Roman_to_Integer {
+    public static void main(String[] args) {
+        Roman_to_Integer roman_to_integer = new Roman_to_Integer();
+        System.out.println(roman_to_integer.romanToInt("IIIM"));
+    }
+
+    static Map<String, Integer> values = new HashMap<>();
+
+    static {
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
+
+    public int romanToInt(String s) {
+
+        int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            int nextValue = 0;
+            if (i + 1 < s.length()) {
+                String nextSymbol = s.substring(i + 1, i + 2);
+                nextValue = values.get(nextSymbol);
+            }
+
+            if (currentValue < nextValue) {
+                sum += (nextValue - currentValue);
+                i += 2;
+            } else {
+                sum += currentValue;
+                i += 1;
+            }
+
+        }
+        return sum;
+    }
+}
