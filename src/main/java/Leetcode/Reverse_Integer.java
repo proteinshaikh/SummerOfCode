03880f661222fe305c5a0823a20589c7da0b68dc
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-05-01 05:55:13 +0530

    leetcode

---
+++ src/main/java/Leetcode/Reverse_Integer.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+
+/* URL: https://leetcode.com/problems/reverse-integer/
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * */
+public class Reverse_Integer {
+    public static void main(String[] args) {
+        Reverse_Integer reverse_integer = new Reverse_Integer();
+        System.out.println(reverse_integer.reverse(123456789));
+    }
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+
