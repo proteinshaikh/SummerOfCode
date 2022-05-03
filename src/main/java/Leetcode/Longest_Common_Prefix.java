62381f3e24ccf71fdfec2c4655f5aa6355a821e4
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-05-03 13:14:49 +0530

    leetcode

---
+++ src/main/java/Leetcode/Longest_Common_Prefix.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+import org.jetbrains.annotations.NotNull;
+
+
+// url: https://leetcode.com/problems/longest-common-prefix/
+public class Longest_Common_Prefix {
+    public static void main(String[] args) {
+        Longest_Common_Prefix longestCommonPrefix = new Longest_Common_Prefix();
+        System.out.println(longestCommonPrefix.longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    public String longestCommonPrefix(String @NotNull [] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 0; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
