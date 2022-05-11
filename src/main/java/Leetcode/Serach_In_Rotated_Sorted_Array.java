a02360d500c6a57204bf33a3547d7d07b6b30e56
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-05-12 05:22:26 +0530

    Create Serach_In_Rotated_Sorted_Array.java

---
+++ src/main/java/Leetcode/Serach_In_Rotated_Sorted_Array.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Serach_In_Rotated_Sorted_Array {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5};
+        System.out.println(search(ints, 4));
+
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+                else start = mid + 1;
+            } else {
+                if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
