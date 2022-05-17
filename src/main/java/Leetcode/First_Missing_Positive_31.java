638e706501a79e0cd22b6ba67bd70404d7f8b238
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-05-18 02:49:09 +0530

    Create First_Missing_Positive_31.java

---
+++ src/main/java/Leetcode/First_Missing_Positive_31.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class First_Missing_Positive_31 {
+    public static void main(String[] args) {
+        int[] ints = {1, 3, 4};
+        System.out.println(firstMissingPositive(ints));
+    }
+
+    public static int firstMissingPositive(int[] A) {
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] == i + 1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if (A[A[i] - 1] != A[i]) swap(A, i, A[i] - 1);
+            else i++;
+        }
+        i = 0;
+        while (i < A.length && A[i] == i + 1) i++;
+        return i + 1;
+    }
+
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
