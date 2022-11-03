ce765b74931ff20c32dc76c275b895456627522e
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2022-11-04 04:52:44 +0530

    Create chartostring.java

---
+++ src/main/java/chartostring.java
@@ -0,0 +1,8 @@
+public class chartostring {
+    public static void main(String args[]) {
+        char chars[] = {'c', 'o', 'd', 'e'};
+        System.out.println(chars);
+        String s = new String(chars);
+        System.out.println(s);
+    }
+}
