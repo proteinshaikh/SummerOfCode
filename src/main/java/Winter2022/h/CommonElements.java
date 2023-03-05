e090e4c04d0ca129c89abe9510b3bd9f6cfb4bfa
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2023-03-05 07:57:37 +0530

    Create CommonElements.java

---
+++ src/main/java/Winter2022/h/CommonElements.java
@@ -0,0 +1,31 @@
+package Winter2022.h;
+
+import java.util.*;
+
+
+//most effitient way
+public class CommonElements {
+
+    static List<Integer> getCommonElements(int[] arr1, int[] arr2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set1.add(arr1[i]);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr2.length; i++) {
+            if (set1.contains(arr2[i])) {
+                list.add(arr2[i]);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> res = getCommonElements((new int[]{1, 2, 3, 4}), (new int[]{1, 2, 3, 4, 6}));
+        for (int a : res) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+}
