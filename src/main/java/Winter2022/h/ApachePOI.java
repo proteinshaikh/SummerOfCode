1933a7a634a612902245794cdc7a10f23f1700e5
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2023-03-07 04:10:33 +0530

    Update ApachePOI.java

--- src/main/java/Winter2022/h/ApachePOI.java
+++ src/main/java/Winter2022/h/ApachePOI.java
@@ -1,19 +1,14 @@
 package Winter2022.h;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 
 /*
-* Apache POI is a Java library for working with Microsoft Office documents such as Excel, Word, and PowerPoint
-* */
+ * Apache POI is a Java library for working with Microsoft Office documents such as Excel, Word, and PowerPoint
+ * */
 public class ApachePOI {
 
     static void excelOPS() throws IOException {
@@ -28,7 +23,33 @@ public class ApachePOI {
         int rowCounter = 0;
         int columnCounter = 0;
 
+        //iterate through each row in the input sheet
+        for (Row inputrow : inputSheet) {
+            if (inputrow.getCell(0).getStringCellValue().equals(criteria)) {
+                Row outputrow = outputSheet.createRow(rowCounter++);
+
+                columnCounter = 0;
+
+                for (Cell inputcell : inputrow) {
+
+                    Cell outputcell = outputrow.createCell(columnCounter++);
+                    outputcell.setCellValue(inputcell.getStringCellValue());
+                }
+            }
+        }
+        FileOutputStream output = new FileOutputStream(new File("output.xlsx"));
+        workbook.write(output);
+
+        output.close();
+        input.close();
+        ;
+        workbook.close();
+
     }
 
 
 }
+
+
+    }
+
