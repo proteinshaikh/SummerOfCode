0c70a0d6058808569551a045f3bd7b031b0c63af
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2023-03-21 05:22:11 +0530

    singleton

---
+++ src/main/java/Winter2022/i/Tests.java
@@ -0,0 +1,28 @@
+package Winter2022.i;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Tests {
+
+    @Test
+    public void testSingleton() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                ThreadSafeSingleton singleton = ThreadSafeSingleton.getInstance();
+                singleton.doSomething();
+                Assert.assertSame(singleton, ThreadSafeSingleton.getInstance());
+            });
+
+            executorService.shutdown();
+            while (!(executorService.isTerminated())) {
+                Thread.sleep(100);
+            }
+        }
+    }
+}
---
+++ src/main/java/Winter2022/i/ThreadSafeSingleton.java
@@ -0,0 +1,29 @@
+package Winter2022.i;
+
+import kotlin.jvm.Synchronized;
+
+class ThreadSafeSingleton {
+
+    private static volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+    }
+
+
+    public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void doSomething(){
+
+    }
+}
+
+
