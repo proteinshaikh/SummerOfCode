9d7746ed15015e773265fc8bf836fc28724fc27e
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2023-03-20 18:15:53 +0530

    Create StringRev.java

---
+++ src/main/java/Winter2022/h/StringRev.java
@@ -0,0 +1,24 @@
+package Winter2022.h;
+
+public class StringRev {
+    public static void main(String[] args) {
+        System.out.println(reverseString("zeeshan shaikh %#"));
+    }
+
+
+    static String reverseString(String str) {
+
+        char[] chars = str.toCharArray();
+        int len = chars.length;
+
+        char[] tempChars = new char[len];
+
+        for (int i = 0; i < len; i++) {
+            tempChars[i] = chars[len - i - 1];
+        }
+
+        String res = new String(tempChars);
+
+        return res;
+    }
+}
