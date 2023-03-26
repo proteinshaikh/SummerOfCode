d71d20aee598eff2d555232ab64f535e410f1419
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2023-03-27 03:08:20 +0530

    lombok

---
+++ src/main/java/Winter2022/i/Employee.java
@@ -0,0 +1,17 @@
+package Winter2022.i;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+
+
+}
